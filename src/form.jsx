ba8d6df636a82e6bd8fa8aaac5a7d789e189a763
Tparfaite <parfaitetwagira@gmail.com>
2022-08-29 21:02:58 +0200

    Feat:useState & useEffect -Add changes made [Finishes#]

--- src/form.jsx
+++ src/form.jsx
@@ -1,13 +1,28 @@
-import react from "react";
+import react, { useEffect, useState } from "react";
 
 const Form=()=>{
-
+const[count,setCount]=useState(4);
+function decrement(){
+    setCount(prevCount=>prevCount -4)
+}
+function increment(){
+setCount(prevCount=>prevCount +1)
+}
+useEffect(()=>{
+    setTimeout(()=>{
+        setCount(prevCount=>prevCount +1)
+    },1000)
+},[])
     return(
         <>
 
         <form action="">
             <input type="text" placeholder="name"></input>
         </form>
+
+      <button onClick={decrement}>-</button>
+      <span>{count}</span>
+      <button onClick={increment}>+</button>
         </>
     )
 
